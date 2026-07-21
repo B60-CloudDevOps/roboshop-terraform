ea9de7d34d708992891d3202b3ff6344af60d767
CodingManoj <mvp2612@hotmail.com>
2026-07-21 16:56:50 -0400

    Provisions Github Runner Tool + testing execution 04

--- tools/backend.tf
+++
@@ -1,4 +0,0 @@
-# Backend attribute is moved to env specific tfvars file, so that we can have different backend for different environment. This is required when we want to maintain separate state file for each environment in same S3 bucket.
-terraform {
-  backend "s3" {}
-}
