eb4ae945dd85f7942f4991cda52854fdda99d16d
CodingManoj <mvp2612@hotmail.com>
2026-07-22 22:21:28 -0400

    Infra

---
+++ modules/ec2/iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "test_role" {
+  name = "${var.name}-${var.env_name}-ec2-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.name}-${var.env_name}-ec2-role"
+  }
+}
