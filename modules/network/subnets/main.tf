74302d1693f4332a26cf5358b022907485a6ef14
CodingManoj <mvp2612@hotmail.com>
2026-09-02 21:35:17 -0400

    Building VPC For Roboshop Infra

--- modules/network/subnets/main.tf
+++ modules/network/subnets/main.tf
@@ -1,3 +1,9 @@
+locals {
+  availability_zones = [
+    for az in var.availability_zones : substr(az, -2, 2)
+  ]
+}
+
 resource "aws_subnet" "main" {
   count             = length(var.cidr_block)
   vpc_id            = var.vpc_id
@@ -5,6 +11,6 @@ resource "aws_subnet" "main" {
   availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
 
   tags = {
-    Name = "roboshop-${var.env}-${var.name}-subnet-${count.index}"
+    Name = "roboshop-${var.env}-${var.name}-subnet-${local.availability_zones[count.index % length(local.availability_zones)]}"
   }
 }
