b2480d2d40995676ce8a89af91fab5fd976b229a
CodingManoj <mvp2612@hotmail.com>
2026-09-02 21:40:09 -0400

    Building VPC For Roboshop Infra

--- modules/network/subnets/main.tf
+++ modules/network/subnets/main.tf
@@ -14,3 +14,13 @@ resource "aws_subnet" "main" {
     Name = "roboshop-${var.env}-${var.name}-subnet-${local.availability_zones[count.index % length(local.availability_zones)]}"
   }
 }
+
+resource "aws_route_table" "main" {
+  count = length(var.cidr_block)
+
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "roboshop-${var.env}-${var.name}-route-table-${local.availability_zones[count.index % length(local.availability_zones)]}"
+  }
+}
